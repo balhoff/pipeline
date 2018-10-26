90ad0fa57751bf07cb14d0d0b063d3645891ec53
Shalsh <shrivastava.shal@gmail.com>
2018-10-26 17:29:53 -0400

    Add some useful comments about logging

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+#squeue -u spshriva ##to check the machine ip on which job is being run
+#error - file containing error log
+#slurm<id>.out - contains output log
+
 sbatch kb_init.sh
