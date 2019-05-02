6c5bf14fba6d12f056dc13fdb82643060da851a4
Shalsh <shrivastava.shal@gmail.com>
2019-05-02 15:52:08 -0400

    Add useful comments

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-#squeue -u spshriva ##to check the machine ip on which job is being run
-#error-<id> - file containing error log
-#slurm-<id>.out - contains output log
+#Covenience script to run the job
+
+#Note:
+##squeue -u spshriva ##to check the machine ip on which job is being run
+##error-<id> - file containing error log
+##slurm-<id>.out - contains output log
 
 sbatch --mail-user=spshriva@renci.org ./slurm-script/kb_init.sh
