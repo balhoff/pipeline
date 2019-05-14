380a12c188d165f46cadbbf4409a7c97b1932df9
Shalsh23 <shrivastava.shal@gmail.com>
2019-05-14 12:46:27 -0400

    Modify slurm script to use particular node for job for speedup

--- kb_init.sh
+++ kb_init.sh
@@ -3,9 +3,9 @@
 #SBATCH --error=error
 #SBATCH --mail-user=spshriva@renci.org
 #SBATCH --mail-type=ALL
-#SBATCH --cpus-per-task=20
-#SBATCH --mem=75G
-
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=250G
+#SBATCH --constraint=broadwell
 set -e # Abort if any command fails
 
 export JAVA_OPTS="-Xmx70G"
