bfc4c695e5962d9cc66906573771b85984edf266
Shalsh <shrivastava.shal@gmail.com>
2019-07-01 19:04:19 -0400

    Write test script

--- test_script.sh
+++ test_script.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
-BIO_ONTOLOGIES="test-bio-ontologies.ofn"
-NEXML_DATA="test-phenoscape-data.ofn"
-# NEXMLS="" #FIXME
+
+TEST_DIR="test/test1"
+BUILD_DIR="build"
 # MONARCH_DATA="test-monarch-data" #FIXME
 
+make clean
+
+cp $(TEST_DIR)/test1-bio-ontologies-merged.ofn $(BUILD_DIR)/bio-ontologies-merged.ofn
+cp $(TEST_DIR)/test1-ontology-metadata.ttl $(BUILD_DIR)/ontology-metadata.ttl
+cp -a $(TEST_DIR)/nexml-data/. $(BUILD_DIR)/test-phenoscape-data/
+
+NEXMLS=$(find $(BUILD_DIR)/test-phenoscape-data/ -type f -name "*.xml")
+
 make all
+
+
 
