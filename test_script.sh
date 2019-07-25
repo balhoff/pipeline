a0cdd651c48827b3c848adf0b4aa31a4d73c0081
Shalsh <shrivastava.shal@gmail.com>
2019-07-25 13:09:50 -0400

    Prevent mirroring of ontologies locally

--- test_script.sh
+++ test_script.sh
@@ -9,11 +9,11 @@ BUILD_DIR="build"
 rm -rf $BUILD_DIR
 
 mkdir $BUILD_DIR
+mkdir $BUILD_DIR/mirror
 cp $TEST_DIR/test1-bio-ontologies-merged.ofn $BUILD_DIR/bio-ontologies-merged.ofn
 cp $TEST_DIR/test1-ontology-metadata.ttl $BUILD_DIR/ontology-metadata.ttl
 cp -a $TEST_DIR/nexml-data/. $BUILD_DIR/test-phenoscape-data/
 
-mkdir $BUILD_DIR/mirror
 
 NEXMLS=$(find $BUILD_DIR/test-phenoscape-data -type f -name "*.xml")
 #echo $NEXMLS
