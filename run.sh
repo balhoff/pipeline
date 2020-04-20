0094b4db71662f02a995d3a5a760a91ae37af010
Shalsh <shrivastava.shal@gmail.com>
2020-04-20 16:37:03 -0400

    Update usage example

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 # cd pipeline_dir
 # docker pull phenoscape/pipeline-tools:latest
 
+# Usage example
+# ./run.sh make all
+
 docker run --volume "$(pwd)":/pipeline --workdir /pipeline --rm -ti phenoscape/pipeline-tools "$@"
 
-# cd ../pipeline
-# make all
