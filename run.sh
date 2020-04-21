9a48c0294d1c6a629a808fb0b9fd3b59eba8f6e8
Shalsh <shrivastava.shal@gmail.com>
2020-04-21 11:36:10 -0400

    Add some useful comments

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
-# cd pipeline_dir
-# docker pull phenoscape/pipeline-tools:latest
+# Convenience script to launch docker container
+# Launches container with interactive bash shell
+# And runs command passed in by the user
 
 # Usage example
 # ./run.sh make all
+
+
+# cd pipeline_dir
+# docker pull phenoscape/pipeline-tools:latest
 
 docker run --volume "$(pwd)":/pipeline --workdir /pipeline --rm -ti phenoscape/pipeline-tools "$@"
 
