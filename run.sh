91a24384752307b7dd4cfc8ba24dcc6ff95aaa80
Shalsh <shrivastava.shal@gmail.com>
2020-04-22 15:43:03 -0400

    Specify docker image version to launch in run script

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Convenience script to launch docker container
 # Launches container with interactive bash shell
 # And runs command passed in by the user
@@ -5,9 +7,10 @@
 # Usage example
 # ./run.sh make all
 
+IMAGE_VERSION=v1.0.2
 
 # cd pipeline_dir
-# docker pull phenoscape/pipeline-tools:latest
+# docker pull phenoscape/pipeline-tools:$IMAGE_VERSION
 
-docker run --volume "$(pwd)":/pipeline --workdir /pipeline --rm -ti phenoscape/pipeline-tools "$@"
+docker run --volume "$(pwd)":/pipeline --workdir /pipeline --rm -ti phenoscape/pipeline-tools:$IMAGE_VERSION "$@"
 
